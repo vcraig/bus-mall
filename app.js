16d89123c0ec9f2633a2e9159a4d7b1cf2592374
Valerie Craig <valine@gmail.com>
2016-06-28 17:30:04 -0700

    app.js, improve and test click and display counters

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Bus Mall
-
 // global variables
 var productArray = [];
 var totalClicksCounter = 0;
@@ -12,14 +11,13 @@ function Product(name, path) {
   this.clickCount = 0;
   productArray.push(this);    // push objects into productArray
 }
-
 var bag = new Product('bag','img/bag.jpg');
 var banana = new Product('banana','img/banana.jpg');
 var bathroom = new Product('bathroom','img/bathroom.jpg');
 var boots = new Product('boots','img/boots.jpg');
-var banana = new Product('usb','img/usb.jpg');
-var bathroom = new Product('water-can','img/water-can.jpg');
-var boots = new Product('wine-glass','img/wine-glass.jpg');
+var usb = new Product('usb','img/usb.jpg');
+var water_can = new Product('water-can','img/water-can.jpg');
+var wine_glass = new Product('wine-glass','img/wine-glass.jpg');
 // add all other pic variables here
 
 // display 3 random pics in a row
@@ -28,22 +26,23 @@ function show3Pics() {
   var pic1st = document.getElementById('pic1st');
   var pic1stRand = randIndex();
   pic1st.src = productArray[pic1stRand].path;
+  pic1st.alt = productArray[pic1stRand].name;
   productArray[pic1stRand].displayCount += 1;
-  console.log(productArray[pic1stRand].name + ' displayCount is ' + productArray[pic1stRand].displayCount + ' and clickcount is ' + productArray[pic1stRand].clickCount);
+  pic1stImgName = productArray[pic1stRand].name;
 //pic2nd
   var pic2nd = document.getElementById('pic2nd');
   var pic2ndRand = randIndex();
   pic2nd.src = productArray[pic2ndRand].path;
+  pic2nd.alt = productArray[pic2ndRand].name;
   productArray[pic2ndRand].displayCount += 1;
-  // console.log(productArray[pic2ndRand].name + ' displayCount is ' + productArray[pic2ndRand].displayCount + ' and clickcount is ' + productArray[pic2ndRand].clickCount);
+  pic2ndImgName = productArray[pic2ndRand].name;
 //pic3rd
   var pic3rd = document.getElementById('pic3rd');
   var pic3rdRand = randIndex();
   pic3rd.src = productArray[pic3rdRand].path;
+  pic3rd.alt = productArray[pic3rdRand].name;
   productArray[pic3rdRand].displayCount += 1;
-  // console.log(productArray[pic3rdRand].name + ' displayCount is ' + productArray[pic3rdRand].displayCount + ' and clickcount is ' + productArray[pic3rdRand].clickCount);
-
-  // picUsedPrior();
+  pic3rdImgName = productArray[pic3rdRand].name;
 }
 show3Pics();
 
@@ -51,16 +50,19 @@ show3Pics();
 var imageContainer = document.getElementById('image-container');
 imageContainer.addEventListener('click', handleImageClick);
 
-// on click
+// click handler
 function handleImageClick(event) {
-  //add more things -- tally total clicks, which pic clicked, incerment number times each pic shown  -- SEE CODE PEN
+  for (var i = 0; i < productArray.length; i++) {
+    if (event.target.alt === productArray[i].name) {
+      // console.log('event.target.alt === ',productArray[i].name);
+      productArray[i].clickCount += 1;
+      console.log('productArray[i].clickCount ', productArray[i].clickCount);
+    }
+  }
   if (totalClicksCounter < 25) {
     show3Pics();
     totalClicksCounter++;
     console.log('Total click count is ' + totalClicksCounter);
-    productArray[pic2ndRand].clickCount += 1;
-    console.log(productArray[pic1stRand].name + ' and clickcount is ' + productArray[pic1stRand].clickCount);
-    //need to associtate the click event with a specific products name and clickCount properties.
 
   }
   else {
